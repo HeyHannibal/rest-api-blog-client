fa3cbcb8f74daa54b998a1c0ca0212ba8909e947
heyhannibal <hannibal.inq@gmail.com>
2022-05-13 00:35:52 +0300

    added homepage reveal text animation

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import uniqid from 'uniqid';
 import { Link } from "react-router-dom";
-
+import '../stylesheets/homepage.css'
 
 export default function Homepage(props) {
 
@@ -9,27 +9,41 @@ export default function Homepage(props) {
 
 
     useEffect(() => {
-        if(!articles) {fetch('http://localhost:3001/article')
-            .then(result => result.json())
-            .then(result => setArticles(JSON.parse(result)))
-    }})
+        if (!articles) {
+            fetch('http://localhost:3001/article')
+                .then(result => result.json())
+                .then(result => setArticles(JSON.parse(result)))
+        }
+    })
 
+    function slideDown(event) {
+        event.currentTarget.lastChild.classList.replace('hidden', 'shown')
+        event.currentTarget.lastChild.firstChild.classList.toggle('shown')
+
+    }
+    function slideUp(event) {
+        event.currentTarget.lastChild.classList.replace('shown', 'hidden')
+        event.currentTarget.lastChild.firstChild.classList.toggle('shown')
+
+    }
     return (
         <div id='homepage'>
-              
-               {articles ? articles.map((article) => 
 
-            
-               <div className='articleLink'>
-                   <p className='date'>{article.date.split('T')[0]}</p>
-                   <Link key={uniqid()} to={article.url} className='articleLink'>{article.title}</Link>
-                    <p className='articlePreview'>{article.body.slice(0,150)}...</p>
-               </div>
-               
-               
-               
-               
-               ) :
+            {articles ? articles.map((article) =>
+
+
+                <div className='articleLink' onMouseEnter={slideDown} onMouseLeave={slideUp}>
+                    <p className='date'>{article.date.split('T')[0]}</p>
+                    <Link key={uniqid()} to={article.url} className='articleLink'>{article.title}</Link>
+                    <p className='textbite'>{article.body.split('.')[0]}.</p>
+                    <div className='articlePreview hidden'><p className='hiddenText'>{article.body.slice(100, 200)}...</p></div>
+
+                </div>
+
+
+
+
+            ) :
                 'Loading articles'}
         </div>
     )
